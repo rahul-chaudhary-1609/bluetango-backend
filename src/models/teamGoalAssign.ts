ec28ffcce56f6cc119384d7ef184a627c75edab0
Nikhil-Gupta <nikhil.gupta@mobicules.com>
2021-02-08 12:39:02 +0530

    goal model update

--- src/models/teamGoalAssign.ts
+++ src/models/teamGoalAssign.ts
@@ -21,6 +21,12 @@ export const teamGoalAssignModel: any = sequelize.define("team_goal_assign", {
         type: DataTypes.INTEGER,
         allowNull: false,
         defaultValue: 0
+    },
+    status: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        comment: "0=>request, 1=> approve, 2=> rejected"
     }
 },
     {
