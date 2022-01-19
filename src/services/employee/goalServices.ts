aacee0e18d0632ca18ec44677a8b60cbe8898dad
Rahul Chaudhary <rahul.chaudhary@algoworks.com>
2022-01-19 20:24:33 +0530

    bugfix

--- src/services/employee/goalServices.ts
+++ src/services/employee/goalServices.ts
@@ -726,6 +726,7 @@ export class GoalServices {
             })
         }
 
+        quantitativeStats.sort((a,b)=>b.quantitative_stats_percent-a.quantitative_stats_percent);
 
 
         return {quantitativeStats};
